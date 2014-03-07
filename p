616fe337a1e73b0c4187ff4bddbcce50dfcd031e
peter <petergarnaes@gmail.com>
2014-03-07 12:32:24 +0100

    Test framework 1.0 done

--- p
+++
@@ -1,1 +0,0 @@
-asjkdasjkfaf
--- test/test-script.sh
+++ test/test-script.sh
@@ -1,64 +1,95 @@
 #!/bin/bash
 atest="test"
 benchmark="benchmark"
-if [[ $1 = $atest ]];
-    then
-    for v in test-files/*-test.rs; do
-            echo $v
-            rustc $v
-    done
-	for thing in test-files/*-test 
-        do
-		    ./$thing
-		    if [[ $? -ne 0 ]]; then
-			    echo "$thing failed to complete"
-			    exit 1
-		    fi
-	done
+if [[ $1 = $atest ]]; then
+    #If file exists
+    if [ -f test-files/*-tests.rs ]; then
+        for v in test-files/*-tests.rs; do
+                echo $v
+                rustc $v
+                if [[ $? -ne 0 ]]; then
+                    echo "$v failed to compile"
+                fi
+                mv $v "${$v}.ignore"
+        done
+        else
+            echo "No tests to compile"
+    fi
+    if [ -f test-files/*-tests ]; then
+	    for thing in test-files/*-tests
+            do
+		        ./$thing
+		        if [[ $? -ne 0 ]]; then
+			        echo "$thing failed to complete"
+			        exit 1
+		        fi
+	    done
+        else
+            echo "No tests to run"
+    fi
 else 
     if [[ $1 = $benchmark ]]; then
-		#Compiling test files
-		echo "Compiling rust test ..."
-        for v in benchmark-files/*-test.rs; do
-            rustc $v
-        done
+        #If file exists
+        if [ -f benchmark-files/*-tests.rs ]; then
+		    echo "Compiling rust test ..."
+            for v in benchmark-files/*-tests.rs; do
+                rustc $v
+                #If error of any kind
+                if [[ $? -ne 0 ]]; then
+                    echo "$v failed to compile.. Error: $?"
+                fi
+                mv $v "${v}.ignore"
+            done
+            else
+                echo "No tests to compile"
+        fi
 
-		echo "Running test"
-		for v in benchmark-files/*-test; do
-            ./$v
-            if [[ $? -ne 0 ]]; then
-                echo "$v program has an error"
-                exit 1
-            fi
-        done
+        if [ -f benchmark-files/*-tests ]; then
+		    echo "Running test"
+		    for v in benchmark-files/*-tests; do
+                ./$v
+                if [[ $? -ne 0 ]]; then
+                    echo "$v program has an error"
+                    exit 1
+                fi
+            done
+            else
+                echo "No test to run"
+        fi
 
-		latexfigure_start = '\begin{figure}\includegraphics[scale=1]{graph-pictures/'
-		latexfigure_end = '}\end{figure}'
+		latexfigure_start='\begin{figure}\includegraphics[scale=0.5]{'
+		latexfigure_end='}\end{figure}'
 	
 		#Outputs a .*-test-graph.png file for each test output
-		echo "Compiling graphs with python script..."
-        for file in benchmark-files/output-files/*-output 
-            do
-		        python graphs.py $file
-        done
+        if [ -f benchmark-files/output-files/*-output ]; then
+		    echo "Compiling graphs with python script..."
+            for file in benchmark-files/output-files/*-output 
+                do
+		            python graphs.py $file
+            done
+            else
+                echo "No output files to graph"
+        fi
 		#Deletes last latex line containing \end{document}
 		echo "Starting latex editing"
 		cp latex-template.tex graphs.tex
-		sed '$d' < graphs.tex
+        #Doesn't work so just removed the line manually
+		sed '$d' < graphs.tex 1> /dev/null
 	
 		#appends each include graphics statment to the tex file
 		for graph in graph-pictures/*-graph.png 
             do
+                echo "Do we have a matchDo we have a match??"
 		        echo $latexfigure_start$graph$latexfigure_end>>graphs.tex
 		done
 	
 		#Ends latex file
 		echo "Ending latex file"
-		echo "\end{document}">>graphs.tex
+		echo '\end{document}'>>graphs.tex
 		echo "Compiling latex..."
-		pdflatex graphs.tex
+		pdflatex graphs.tex 1> /dev/null
 		echo "Cleaning up"
-		#rm graphs.tex
+		rm graphs.tex graphs.aux graphs.log
 		echo "Opening pdf for your pleasure!"
 		evince graphs.pdf
 	fi
