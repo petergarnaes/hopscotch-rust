c93b9fe81f826915f9b5cbaa5d7dfd846defae1a
peter <petergarnaes@gmail.com>
2014-03-07 02:08:57 +0100

    Test framework script is mostly done, sed not removing line though, and things are not being concattenated to file, maybe a proper test will work...

--- test/graphs.py
+++ test/graphs.py
@@ -3,11 +3,15 @@ import sys
 
 filename = sys.argv[1]
 theFile = open(filename,'r')
-testOutput = theFile.read()
-inputString = testOutput.split()
-inputInt = map(int,inputString)
-line_chart = pygal.Line()
-line_chart.title = 'Test'
-line_chart.add('Test',inputInt)
-print inputInt
-line_chart.render_to_png('a-test-graph.png')
+testOutput = theFile.readlines()
+theTitle = testOutput[0]
+xAxisTitle = testOutput[1]
+yAxisTitle = testOutput[2]
+inputX = testOutput[3].split()
+inputY = testOutput[4].split()
+line_chart = pygal.Line(title=theTitle,x_title=xAxisTitle,y_title=yAxisTitle)
+line_chart.x_labels = inputX
+line_chart.add('Test',map(int,inputY))
+#Clean up prefix
+prefix = "a"
+line_chart.render_to_png('graph-pictures/'+prefix+'-graph.png')
--- test/test-script.sh
+++ test/test-script.sh
@@ -1,33 +1,65 @@
 #!/bin/bash
+atest="test"
+benchmark="benchmark"
+if [[ $1 = $atest ]];
+    then
+    for v in test-files/*-test.rs; do
+            echo $v
+            rustc $v
+    done
+	for thing in test-files/*-test 
+        do
+		    ./$thing
+		    if [[ $? -ne 0 ]]; then
+			    echo "$thing failed to complete"
+			    exit 1
+		    fi
+	done
+else 
+    if [[ $1 = $benchmark ]]; then
+		#Compiling test files
+		echo "Compiling rust test ..."
+        for v in benchmark-files/*-test.rs; do
+            rustc $v
+        done
 
-#Compiling test files
-echo "Compiling rust test ..."
-rustc rust-test.rs
+		echo "Running test"
+		for v in benchmark-files/*-test; do
+            ./$v
+            if [[ $? -ne 0 ]]; then
+                echo "$v program has an error"
+                exit 1
+            fi
+        done
 
-latexfigure_start = '\begin{figure}\includegraphics[scale=1]{'
-latexfigure_end = '}\end{figure}'
-
-#Outputs a .*-test-graph.png file for each test output
-echo "Compiling graphs with python script..."
-python graph.py .*-test-output
-
-#Deletes last latex line containing \end{document}
-echo "Starting latex editing"
-sed '$d' graphs.tex
-
-#appends each include graphics statment to the tex file
-for graph in .*-test-graph.png do
-    echo "$latexfigure_start$graph$latexfigure_end">>graphs.tex
-done
-
-#Ends latex file
-echo '\end{document}'>>graphs.tex
-echo "Latex finished..."
-echo "Compiling latex..."
-pdflatex graphs.tex
-echo "Mobing pdf out of test dir..."
-mv graphs.pdf ../graphs.pdf
-echo "Moving out of test dir..."
-cd ../
-echo "Opening pdf for your pleasure!"
-evince graphs.pdf
+		latexfigure_start = '\begin{figure}\includegraphics[scale=1]{graph-pictures/'
+		latexfigure_end = '}\end{figure}'
+	
+		#Outputs a .*-test-graph.png file for each test output
+		echo "Compiling graphs with python script..."
+        for file in benchmark-files/output-files/*-output 
+            do
+		        python graphs.py $file
+        done
+		#Deletes last latex line containing \end{document}
+		echo "Starting latex editing"
+		cp latex-template.tex graphs.tex
+		sed '$d' < graphs.tex
+	
+		#appends each include graphics statment to the tex file
+		for graph in graph-pictures/*-graph.png 
+            do
+		        echo $latexfigure_start$graph$latexfigure_end>>graphs.tex
+		done
+	
+		#Ends latex file
+		echo "Ending latex file"
+		echo "\end{document}">>graphs.tex
+		echo "Compiling latex..."
+		pdflatex graphs.tex
+		echo "Cleaning up"
+		#rm graphs.tex
+		echo "Opening pdf for your pleasure!"
+		evince graphs.pdf
+	fi
+fi
