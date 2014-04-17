72e3698a8e8975d9b23afec7d5b3918abf8f0715
Peter Alexander Garnæs <petergarnaes@gmail.com>
2014-04-17 09:04:23 +0200

    test hashmap skeleton

---
+++ test/test-files/hash_map.rs
@@ -0,0 +1,54 @@
+//#[path="../../hashmap/mod.rs"]
+//mod hopscotch;
+extern crate collections;
+
+#[cfg(test)]
+mod test_hopscotch{
+    use collections::hashmap::HashMap;
+    //super::hopscotch::{HashMap};
+
+    #[test]
+    fn test_lookup_no_insert(){
+
+    }
+    #[test]
+    fn test_lookup_all_slots(){
+
+    }
+    #[test]
+    fn test_insert_no_lookup(){
+
+    }
+    #[test]
+    fn test_insert_all_slots(){
+
+    }
+    #[test]
+    fn test_remove_of_an_invalid_key(){
+
+    }
+    #[test]
+    fn test_remove_of_valid_key(){
+
+    }
+    #[test]
+    fn test_remove_all_slots(){
+
+    }
+    #[test]
+    fn test_resize(){
+
+    }
+    #[test]
+    fn test_insert_after_resize(){
+
+    }
+    #[test]
+    fn test_lookup_after_resize(){
+
+    }
+    #[test]
+    fn test_remove_after_resize(){
+
+    }
+}
